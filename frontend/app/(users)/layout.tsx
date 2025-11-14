51e639147395a402476c1977f354f71473eac89b
Abu Horaira <syedmdabuhoraira@gmail.com>
2025-11-14 14:44:03 +0600

    deploy

--- frontend/app/(users)/layout.tsx
+++ frontend/app/(users)/layout.tsx
@@ -1,5 +1,13 @@
-const UserLayout = async ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
+import { ReactNode } from "react";
+
+export const dynamic = "force-dynamic";
+
+interface UserLayoutProps {
+  children: ReactNode;
+}
+
+const UserLayout = ({ children }: UserLayoutProps) => {
+  return children;
 };
 
 export default UserLayout;
